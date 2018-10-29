d3dd65901fac202cb163dbe74f0805d714dedeb8
Zvonimir Loncaric <zvonimir.loncaric1@gmail.com>
2018-10-29 16:39:40 +0100

    prepare for serving static files

--- backend-api/src/routes/test.js
+++ backend-api/src/routes/test.js
@@ -1,7 +1,7 @@
 const Test = require('../controllers/test');
 const {wrapAsync} = require('../utils/controllers');
 
-module.exports = api => {
-    api.route('/ping/public').get(wrapAsync(Test.pingPublic));
-    api.route('/ping/secure').get(wrapAsync(Test.pingSecure));
+module.exports = (api, base_path) => {
+    api.route(base_path + '/ping/public').get(wrapAsync(Test.pingPublic));
+    api.route(base_path + '/ping/secure').get(wrapAsync(Test.pingSecure));
 };
