b40e311c979b383d3345ab90448846bd70154bbf
Zvonimir Loncaric <zvonimir.loncaric1@gmail.com>
2018-11-26 02:25:02 +0100

    extend user api endpoints with identifiers of subscribed vehicles (per user)

--- backend-api/src/controllers/user.js
+++ backend-api/src/controllers/user.js
@@ -1,15 +1,27 @@
-const User = require('../models/user').user;
-
+const db = require('../models/user/index')
 
 const getById = async (req, res) => {
-  const { userId } = req.params.userId;
-  User.findOne({ where: { id: userId } }).then((item) => {
-    res.status(200).json(item);
-  });
+  const { userId } = req.params;
+  db.sequelize.query(
+    `${'(SELECT * , ' +
+        '(SELECT CONCAT("[", group_concat(DISTINCT vin ORDER BY vin SEPARATOR ","),"]")'
+        + 'FROM `user_vehicle`'
+        + 'WHERE `user_vehicle`.`userId` = `u`.`id`)'
+    + 'AS `vehicles` FROM `user` AS `u` WHERE `u`.`id` = '}${ 
+     String(userId)  })`,
+    { type: db.sequelize.QueryTypes.SELECT }
+  ).then(items => res.status(200).json(items));
 };
 
 const getAll = async (req, res) => {
-  User.findAll().then(items => res.status(200).json(items));
+  db.sequelize.query(
+    '(SELECT * , ' +
+        '(SELECT CONCAT("[", group_concat(DISTINCT vin ORDER BY vin SEPARATOR ","),"]")'
+        + 'FROM `user_vehicle`'
+        + 'WHERE `user_vehicle`.`userId` = `u`.`id`)'
+    + 'AS `vehicles` FROM `user` AS `u`)',
+    { type: db.sequelize.QueryTypes.SELECT }
+  ).then(items => res.status(200).json(items));
 };
 
 module.exports.getById = getById;
