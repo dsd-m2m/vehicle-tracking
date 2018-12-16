3c468cff1fd05b2570bd4101ddd6dfb31b430930
Zvonimir Loncaric <zvonimir.loncaric1@gmail.com>
2018-12-16 21:24:29 +0100

    srv export in csv
    - exporting dynamodb data in csv format (as an attachment)
    - enabled query params (starttime, endtime, projection fields)
    - csv file is named like this: "<vin>-<starttime>-<endtime>"
    - if no data is selected with the query, csv with length 0 is returned

--- backend-api/src/controllers/sensorData.js
+++ backend-api/src/controllers/sensorData.js
@@ -2,6 +2,7 @@
 /* eslint-disable consistent-return */
 const _ = require('lodash');
 const moment = require('moment');
+const json2csv = require('json2csv');
 const SensorData = require('../models/sensor/sensorData');
 
 const get = async (req, res, next) => {
@@ -22,6 +23,47 @@ const get = async (req, res, next) => {
       }
       return res.status(200).json(_.map(resp.Items, 'attrs'))
     });
+};
+
+const exportCsv = async (req, res, next) => {
+  const { vin } = req.params;
+  const field = req.query.field;
+
+  if (!vin) {
+    return res.status(400).json({ message: 'Undefined vehicle id' });
+  }
+  const start = parseInt(req.query.start, 10) || 0;
+  const end = parseInt(req.query.end, 10) || moment().valueOf();
+
+  let call;
+  if (field) {
+    call = SensorData
+      .query(vin)
+      .where('timestamp').between(start, end)
+      .attributes(field);
+  } else {
+    call = SensorData
+      .query(vin)
+      .where('timestamp').between(start, end);
+  }
+  call.exec((err, resp) => {
+    if (err) {
+      return next();
+    }
+    let data = _.map(resp.Items, 'attrs');
+
+    
+    const filename = `${vin}-${start}-${end}.csv`;
+    if (data.length !== 0) {
+      data = json2csv.parse(data);
+    }else{
+      data = undefined;
+    }
+
+    res.setHeader('Content-disposition', `attachment; filename=${filename}`);
+    res.set('Content-Type', 'text/csv');
+    res.status(200).send(data);
+  });
 };
 
 const all = async (req, res, next) => {
@@ -49,4 +91,4 @@ const all = async (req, res, next) => {
   });
 };
 
-module.exports = { get, all };
+module.exports = { get, all, exportCsv };
