50c2dfbdc89e2d7f4f559bb5a09493b71ab015a2
Tomislav Skokovic <tomislav.skokovic@comsysto.com>
2019-01-08 10:30:06 +0100

    web ui homepage eye candy

--- web/src/HomePage/index.js
+++ web/src/HomePage/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Header from '../_components/Header';
 
+const blueSpan = {
+	color: '#2D9CDB'
+}
+
 
 class HomePage extends React.Component {
 	constructor(props) {
@@ -20,6 +24,22 @@ class HomePage extends React.Component {
 			<div className="" >
 				<Header/>
 				<div className="homepage">
+					<div className="firstCandy">
+						Number of cars <br/><br/>
+						<span style={blueSpan}>1337</span>
+					</div>
+					<div className="secondCandy">
+						Connected cars <br/><br/>
+						<span style={blueSpan}>800</span>
+					</div>
+					<div className="thirdCandy">
+						Fastest vehicle <br/><br/>
+						<span style={blueSpan}>ConceptTwo</span>
+					</div>
+					<div className="fourthCandy">
+						Oldest vehicle <br/><br/>
+						<span style={blueSpan}>ConceptOne</span>
+					</div>
 				</div>
 			</div>
 
